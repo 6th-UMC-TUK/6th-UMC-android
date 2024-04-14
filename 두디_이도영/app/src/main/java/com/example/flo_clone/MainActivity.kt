2e98d9b214bc159c7ab5369de1b972fc590628c2
Dodi029 <doyo0529@tukorea.ac.kr>
2024-04-14 21:50:11 +0900

    feat: 2주차
    액티비티 전환

--- 두디_이도영/app/src/main/java/com/example/flo_clone/MainActivity.kt
+++ 두디_이도영/app/src/main/java/com/example/flo_clone/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.flo_clone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
@@ -14,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // binding 사용중
+        // 원래라면 findById로 접근해야함 -> binding으로 간편하게 사용 가능
+
+        binding.mainPlayerCl.setOnClickListener {
+            // 뷰에 대한 함수
+            startActivity(Intent(this, SongActivity::class.java))
+            // Intent를 사용하여 this에서 SongActivity로 이동
+        }
 
         initView()
     }
