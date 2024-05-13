0cfd7748b14782081bc2bb6b771e369f7c6b4064
ekgml001 <dahea5069@gmail.com>
2024-05-13 23:48:53 +0900

    6주차중

--- 반희_김다희/FLO/app/src/main/java/com/example/flo/AlbumFragment.kt
+++ 반희_김다희/FLO/app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -7,10 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentAlbumBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
 
 class AlbumFragment : Fragment() {
     private lateinit var binding: FragmentAlbumBinding
     private val information = arrayListOf("수록곡", "상세정보", "영상")
+    private val gson: Gson = Gson()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +20,10 @@ class AlbumFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAlbumBinding.inflate(inflater, container, false)
+
+        val albumJson = arguments?.getString("album")
+        val album = gson.fromJson(albumJson, Album::class.java)
+        setInit(album)
 
         binding.albumBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager
@@ -34,5 +40,11 @@ class AlbumFragment : Fragment() {
         }.attach()
 
         return binding.root
+    }
+
+    private fun setInit(album: Album){
+        binding.albumAlbumIv.setImageResource(album.coverImg!!)
+        binding.albumMusicTitleTv.text = album.title.toString()
+        binding.albumSingerNameTv.text = album.singer.toString()
     }
 }
